6590b1569619791ec10e0bd31cd57380fce66023
naufalziyad <naufal.polinema@gmail.com>
2018-06-06 17:03:42 +0700

    update

--- main.go
+++ main.go
@@ -4,14 +4,14 @@ package main
 
 import ("database/sql"
 		"fmt"
-		/*"html/template"*/
+		"html/template"
 		"log"
 		"net/http"
 
 		"golang.org/x/crypto/bcrypt"
 
 		_ "github.com/go-sql-driver/mysql"
-		/*"github.com/kataras/go-sessions"*/)
+		"github.com/kataras/go-sessions")
 
 var db *sql.DB
 var err error
@@ -39,6 +39,9 @@ func connect_db(){
 
 func routes(){
 	http.HandleFunc("/register", register)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/home", home)
+	http.HandleFunc("/logout", logout)
 }
 
 func main(){
@@ -106,14 +109,72 @@ func register(w http.ResponseWriter, r *http.Request){
 	}
 }
 
+func login(w http.ResponseWriter, r *http.Request) {
+	session := sessions.Start(w, r)
+	if len(session.GetString("username")) != 0 && checkErr(w, r, err) {
+		http.Redirect(w, r, "/", 302)
+	}
+
+	if r.Method != "POST" {
+		http.ServeFile(w, r, "login.html")
+		return
+	}
+
+	username := r.FormValue("username")
+	password := r.FormValue("password")
+
+	users := QueryUser(username)
+
+	var password_check = bcrypt.CompareHashAndPassword([]byte(users.Password), []byte(password))
+
+	if password_check == nil {
+		//apabila login berhasil
+		session := sessions.Start(w, r)
+		session.Set("username", users.Username)
+		session.Set("name", users.Firstname)
+		http.Redirect(w, r, "/home", 302)
+	}else{
+		//apabila login gagal
+		http.Redirect(w, r, "/login", 302)
+	}
+}
+
+func home(w http.ResponseWriter, r *http.Request) {
+	session := sessions.Start(w, r)
+	if len(session.GetString("username")) == 0 {
+		http.Redirect(w, r, "/login", 301)
+	}
+
+	var data = map[string]string{
+		"username": session.GetString("username"),
+		"message": "Selamat datang!",
+	}
+
+	var t, err = template.ParseFiles("home.html")
+	if err !=nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	t.Execute(w, data)
+	return
+}
+
+func logout(w http.ResponseWriter, r *http.Request) {
+	session := sessions.Start(w, r)
+	session.Clear()
+	sessions.Destroy(w, r)
+	http.Redirect(w, r, "/login", 302)
+}
+
 func checkErr(w http.ResponseWriter, r *http.Request, err error) bool {
- if err != nil {
+	if err != nil {
 
-fmt.Println(r.Host + r.URL.Path)
+	fmt.Println(r.Host + r.URL.Path)
 
-http.Redirect(w, r, r.Host+r.URL.Path, 301)
- return false
- }
+	http.Redirect(w, r, r.Host+r.URL.Path, 301)
+	return false
+	}
 
 return true
 }
